37078faa9a636feb5bd6334d0737f752d4c203e0
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-07-11 17:02:53 -0700

    Add files via upload

--- USB Drive Formatter.cpp
+++ USB Drive Formatter.cpp
@@ -3,27 +3,29 @@
 #include <windows.h>
 #include <setupapi.h>
 #include <devguid.h>
-#include <iostream>
 
 #pragma comment(lib, "setupapi.lib")
 
-#define MAX_LOADSTRING 100
+#define MAXIMO_CARACTERES 100
 
-// Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+// Variables globales
+HINSTANCE instancia_aplicacion;
+WCHAR titulo_ventana[MAXIMO_CARACTERES] = L"Formateador USB"; // Valor por defecto
+WCHAR clase_ventana[MAXIMO_CARACTERES] = L"ClasePrincipalUSB";
 
-HWND hListBox;  // ListBox to display USB devices
+// Controles
+HWND hComboUnidades;
+HWND hBotonFormatear;
+HWND hBarraProgreso;
+HWND hAreaFormateo;
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-void ListUSBDevices(HWND hWnd);
+// Prototipos de funciones
+ATOM RegistrarClaseVentana(HINSTANCE hInstance);
+BOOL IniciarInstancia(HINSTANCE, int);
+LRESULT CALLBACK ProcesarMensajes(HWND, UINT, WPARAM, LPARAM);
+void CrearControles(HWND hWnd);
+void ListarDispositivosUSB();
 
-// Entry point for the application.
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPWSTR    lpCmdLine,
@@ -32,165 +34,206 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_USBDRIVEFORMATTER, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+    instancia_aplicacion = hInstance;
 
-    // Perform application initialization:
-    if (!InitInstance(hInstance, nCmdShow))
+    // Registrar clase de ventana
+    if (!RegistrarClaseVentana(hInstance))
     {
+        MessageBox(NULL, L"Error registrando clase de ventana", L"Error", MB_ICONERROR);
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_USBDRIVEFORMATTER));
+    // Crear ventana principal
+    HWND hWnd = CreateWindowW(
+        clase_ventana,
+        titulo_ventana,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
 
-    MSG msg;
-
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    if (!hWnd)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        MessageBox(NULL, L"Error creando ventana principal", L"Error", MB_ICONERROR);
+        return FALSE;
+    }
+
+    // Crear controles
+    CrearControles(hWnd);
+
+    // Listar dispositivos
+    ListarDispositivosUSB();
+
+    // Mostrar y actualizar ventana
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    // Bucle principal de mensajes CON DEPURACIÓN
+    MSG msg;
+    while (true)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
         {
+            if (msg.message == WM_QUIT)
+                break;
+
             TranslateMessage(&msg);
             DispatchMessage(&msg);
+        }
+        else
+        {
+            // Mensaje de depuración
+            OutputDebugString(L"Bucle principal ejecutándose...\n");
+            Sleep(10); // Pequeña pausa para evitar uso excesivo de CPU
         }
     }
 
     return (int)msg.wParam;
 }
 
-// Registers the window class.
-ATOM MyRegisterClass(HINSTANCE hInstance)
+ATOM RegistrarClaseVentana(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
-
+    WNDCLASSEXW wcex = { 0 };
     wcex.cbSize = sizeof(WNDCLASSEX);
-
     wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
+    wcex.lpfnWndProc = ProcesarMensajes;
     wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_USBDRIVEFORMATTER));
-    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
     wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_USBDRIVEFORMATTER);
-    wcex.lpszClassName = szWindowClass;
-    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.lpszClassName = clase_ventana;
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
     return RegisterClassExW(&wcex);
 }
 
-// Saves instance handle and creates main window
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+void CrearControles(HWND hWnd)
 {
-    hInst = hInstance; // Store instance handle in our global variable
+    // ComboBox para unidades USB
+    hComboUnidades = CreateWindowW(
+        L"COMBOBOX",
+        L"",
+        WS_CHILD | WS_VISIBLE | WS_VSCROLL | CBS_DROPDOWN | CBS_SORT | WS_TABSTOP,
+        50, 50, 300, 150,
+        hWnd,
+        (HMENU)IDC_COMBO_UNIDADES,
+        instancia_aplicacion,
+        NULL
+    );
 
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    // Botón de formatear
+    hBotonFormatear = CreateWindowW(
+        L"BUTTON",
+        L"Formatear",
+        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        360, 50, 100, 25,
+        hWnd,
+        (HMENU)IDC_FORMATEADOR_USB,
+        instancia_aplicacion,
+        NULL
+    );
 
-    if (!hWnd)
-    {
-        return FALSE;
-    }
+    // Área de información
+    hAreaFormateo = CreateWindowW(
+        L"STATIC",
+        L"",
+        WS_CHILD | WS_VISIBLE | SS_ETCHEDFRAME,
+        50, 100, 500, 300,
+        hWnd,
+        (HMENU)IDC_AREA_FORMATEO,
+        instancia_aplicacion,
+        NULL
+    );
 
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
-
-    ListUSBDevices(hWnd);  // Call the function to list USB devices
-
-    return TRUE;
+    // Barra de progreso vertical
+    hBarraProgreso = CreateWindowW(
+        L"SCROLLBAR",
+        L"",
+        WS_CHILD | WS_VISIBLE | SBS_VERT,
+        560, 100, 20, 300,
+        hWnd,
+        (HMENU)IDC_BARRA_PROGRESO,
+        instancia_aplicacion,
+        NULL
+    );
 }
 
-// Processes messages for the main window.
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+void ListarDispositivosUSB()
+{
+    HDEVINFO hDevInfo = SetupDiGetClassDevs(&GUID_DEVCLASS_USB, NULL, NULL, DIGCF_PRESENT);
+    if (hDevInfo == INVALID_HANDLE_VALUE)
+    {
+        MessageBox(NULL, L"No se pudieron enumerar dispositivos USB", L"Error", MB_ICONERROR);
+        return;
+    }
+
+    SP_DEVINFO_DATA DeviceInfoData = { 0 };
+    DeviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+    for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &DeviceInfoData); i++)
+    {
+        WCHAR deviceName[256];
+        DWORD size = 0;
+
+        if (SetupDiGetDeviceRegistryProperty(
+            hDevInfo,
+            &DeviceInfoData,
+            SPDRP_DEVICEDESC,
+            NULL,
+            (PBYTE)deviceName,
+            sizeof(deviceName),
+            &size))
+        {
+            SendMessage(hComboUnidades, CB_ADDSTRING, 0, (LPARAM)deviceName);
+        }
+    }
+
+    SetupDiDestroyDeviceInfoList(hDevInfo);
+}
+
+LRESULT CALLBACK ProcesarMensajes(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
     case WM_COMMAND:
     {
         int wmId = LOWORD(wParam);
-        // Parse the menu selections:
         switch (wmId)
         {
-        case IDM_ABOUT:
-            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-            break;
-        case IDM_EXIT:
-            DestroyWindow(hWnd);
+        case IDC_FORMATEADOR_USB:
+            MessageBox(hWnd, L"Funcionalidad de formateo será implementada aquí", L"Acción", MB_OK);
             break;
         default:
             return DefWindowProc(hWnd, message, wParam, lParam);
         }
     }
     break;
+
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hWnd, &ps);
+
+        // Dibujar texto en el área de formateo
+        RECT rect;
+        GetClientRect(hAreaFormateo, &rect);
+        DrawText(hdc, L"Información de formateo aparecerá aquí", -1, &rect,
+            DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
         EndPaint(hWnd, &ps);
     }
     break;
+
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
+
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
-}
-
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
-}
-
-// Function to list USB devices
-void ListUSBDevices(HWND hWnd)
-{
-    // Get a list of all USB devices
-    HDEVINFO hDevInfo = SetupDiGetClassDevs(&GUID_DEVCLASS_USB, NULL, NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
-    if (hDevInfo == INVALID_HANDLE_VALUE)
-        return;
-
-    SP_DEVINFO_DATA DeviceInfoData;
-    DeviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
-
-    // Create a list box to display the USB devices
-    hListBox = CreateWindow(L"LISTBOX", L"USB Devices",
-        WS_CHILD | WS_VISIBLE | WS_VSCROLL | LBS_NOTIFY,
-        10, 10, 400, 200, hWnd, (HMENU)1, hInst, NULL);
-
-    int index = 0;
-
-    // Enumerate through the devices
-    for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &DeviceInfoData); i++)
-    {
-        WCHAR szDeviceName[1024];
-        DWORD dwSize = 0;
-
-        // Get the device description
-        SetupDiGetDeviceRegistryProperty(hDevInfo, &DeviceInfoData, SPDRP_DEVICEDESC, NULL, (PBYTE)szDeviceName, sizeof(szDeviceName), &dwSize);
-
-        // Add the device to the listbox
-        SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)szDeviceName);
-    }
-
-    SetupDiDestroyDeviceInfoList(hDevInfo);
 }
---
+++ resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+#pragma once
+
+#define IDC_STATIC                      -1  // Definición añadida
+#define IDD_VENTANA_PRINCIPAL_REFERENCIA 129
+#define IDS_TITULO_APLICACION           103
+#define IDM_SALIR                       105
+#define IDC_FORMATEADOR_USB             109
+#define IDR_PRINCIPAL                   128
+#define IDC_COMBO_UNIDADES              1000
+#define IDC_BARRA_DESPLAZAMIENTO        1001
+#define IDC_AREA_FORMATEO               1002
+#define IDC_BARRA_PROGRESO              1005
+
+// Next default values
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
