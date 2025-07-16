6e194fb0cd6a3d7a614f5aa33ee3afb03581bda9
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-07-16 15:06:15 -0700

    Add files via upload

--- ventana_principal.cpp
+++ ventana_principal.cpp
@@ -2,19 +2,18 @@
 #include <windows.h> // Necesario para HINSTANCE, LPSTR, etc.
 
 using namespace System;
-using namespace System::Windows::Forms;
 
 // El atributo STAThread debe estar antes de la declaración de WinMain
 [STAThread]
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    Application::EnableVisualStyles();
-    Application::SetCompatibleTextRenderingDefault(false);
+    System::Windows::Forms::Application::EnableVisualStyles();
+    System::Windows::Forms::Application::SetCompatibleTextRenderingDefault(false);
 
     // Crear y ejecutar el formulario principal
     DriveFormatter::ventana_principal^ form = gcnew DriveFormatter::ventana_principal();
-    Application::Run(form);
+    System::Windows::Forms::Application::Run(form);
 
     return 0;
 }
--- ventana_principal.h
+++ ventana_principal.h
@@ -16,6 +16,14 @@ namespace DriveFormatter
                 delete components;
             }
         }
+    private: System::Windows::Forms::Label^ label1;
+    private: System::Windows::Forms::ComboBox^ comboBox1;
+    private: System::Windows::Forms::Button^ button1;
+    private: System::Windows::Forms::ProgressBar^ progressBar1;
+    private: System::Windows::Forms::TextBox^ textBox1;
+    private: System::Windows::Forms::VScrollBar^ vScrollBar1;
+
+    protected:
 
     protected:
     protected:
@@ -24,16 +32,77 @@ namespace DriveFormatter
 #pragma region Windows Form Designer generated code
         void InitializeComponent(void)
         {
+            this->label1 = (gcnew System::Windows::Forms::Label());
+            this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+            this->button1 = (gcnew System::Windows::Forms::Button());
+            this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
+            this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+            this->vScrollBar1 = (gcnew System::Windows::Forms::VScrollBar());
             this->SuspendLayout();
+            // 
+            // label1
+            // 
+            this->label1->AutoSize = true;
+            this->label1->Location = System::Drawing::Point(12, 10);
+            this->label1->Name = L"label1";
+            this->label1->Size = System::Drawing::Size(77, 13);
+            this->label1->TabIndex = 0;
+            this->label1->Text = L"Select Device:";
+            // 
+            // comboBox1
+            // 
+            this->comboBox1->FormattingEnabled = true;
+            this->comboBox1->Location = System::Drawing::Point(15, 36);
+            this->comboBox1->Name = L"comboBox1";
+            this->comboBox1->Size = System::Drawing::Size(244, 21);
+            this->comboBox1->TabIndex = 1;
+            // 
+            // button1
+            // 
+            this->button1->Location = System::Drawing::Point(265, 35);
+            this->button1->Name = L"button1";
+            this->button1->Size = System::Drawing::Size(75, 23);
+            this->button1->TabIndex = 2;
+            this->button1->Text = L"Format";
+            this->button1->UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this->progressBar1->Location = System::Drawing::Point(15, 399);
+            this->progressBar1->Name = L"progressBar1";
+            this->progressBar1->Size = System::Drawing::Size(325, 23);
+            this->progressBar1->TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this->textBox1->Location = System::Drawing::Point(15, 69);
+            this->textBox1->Multiline = true;
+            this->textBox1->Name = L"textBox1";
+            this->textBox1->Size = System::Drawing::Size(308, 318);
+            this->textBox1->TabIndex = 4;
+            // 
+            // vScrollBar1
+            // 
+            this->vScrollBar1->Location = System::Drawing::Point(323, 69);
+            this->vScrollBar1->Name = L"vScrollBar1";
+            this->vScrollBar1->Size = System::Drawing::Size(17, 318);
+            this->vScrollBar1->TabIndex = 5;
             // 
             // ventana_principal
             // 
             this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
             this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-            this->ClientSize = System::Drawing::Size(284, 261);
+            this->ClientSize = System::Drawing::Size(354, 434);
+            this->Controls->Add(this->vScrollBar1);
+            this->Controls->Add(this->textBox1);
+            this->Controls->Add(this->progressBar1);
+            this->Controls->Add(this->button1);
+            this->Controls->Add(this->comboBox1);
+            this->Controls->Add(this->label1);
             this->Name = L"ventana_principal";
-            this->Text = L"ventana_principal";
+            this->Text = L"Drive Formatter";
             this->ResumeLayout(false);
+            this->PerformLayout();
 
         }
 #pragma endregion
